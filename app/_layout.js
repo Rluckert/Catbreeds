8348cd9d0776f4153496bdc31fa97c5c91f1360d
Rafael Luckert <rafael.luckert@hotmail.com>
2025-06-14 20:48:30 -0500

    [feat]: added font load when init the app

--- app/_layout.js
+++ app/_layout.js
@@ -1,18 +1,35 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { Stack, SplashScreen } from "expo-router";
 import { View } from "react-native";
 
+// Evita que el splash desaparezca automáticamente
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto: require("../assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
-    <View className="flex-1 bg-black">
+    <View style={{ flex: 1, backgroundColor: "black" }}>
       <Stack
         screenOptions={{
           headerStyle: { backgroundColor: "black" },
-          headerTitle: "Testing", 
-          headerTintColor: "black",
-          headerTitleStyle: { color: "black" },
-          headerLeft: () => {} ,
-          headerRight: () => {},
+          headerTintColor: "white",
+          headerTitleStyle: {
+            color: "white",
+            fontFamily: "Poppins",
+            fontSize: 18,
+          },
         }}
       />
     </View>
