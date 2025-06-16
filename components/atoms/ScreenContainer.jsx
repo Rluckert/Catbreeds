4edb2e24477b6269f1b38e210dd17366d1d19449
Rafael Luckert <rafael.luckert@hotmail.com>
2025-06-16 13:15:31 -0500

    [feat]: improve breed list load

---
+++ components/atoms/ScreenContainer.jsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function ScreenContainer({ children }) {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      className="flex-1 bg-white px-2"
+      style={{ paddingBottom: insets.bottom }}
+    >
+      {children}
+    </View>
+  );
+}
